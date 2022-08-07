8d0a13bb617e55488ad610f8b5723c85ca7cd57c
n7rider <sjoedanny@gmail.com>
2022-08-06 23:44:54 -0400

    Retrieve movie details from database

--- routes/profile.js
+++ routes/profile.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const users = require('../data/users')
+const movies = require('../data/movies')
 
 router.get('/', async (req, res) => {
   // TODO Stop non-users from accessing this, and remove this hard-coding
@@ -8,11 +9,37 @@ router.get('/', async (req, res) => {
   req.session.user = 'john_doe'
   if (true || (req.session && req.session.user)) {
     const user = await users.getUsername(req.session.user)
+    let movieIds = []
+    pushToArray(movieIds, user.watch_list)
+    pushToArray(movieIds, user.preferences.liked_movies)
+    pushToArray(movieIds, user.preferences.disliked_movies)
+
+    const moviesResult = await movies.getMoviesById(movieIds)
+    let moviesResultMap = {}
+    moviesResult.forEach(movie => {
+      moviesResultMap[movie._id.toString()] = movie
+    })
+
+    let watchList = []
+    user.watch_list.map(movie => {
+      watchList.push(moviesResultMap[movie])
+    })
+    let likedMovies = []
+    user.preferences.liked_movies.map(movie => {
+      likedMovies.push(moviesResultMap[movie])
+    })
+    let dislikedMovies = []
+    user.preferences.disliked_movies.map(movie => {
+      dislikedMovies.push(moviesResultMap[movie])
+    })
+
     res.render('users/profile', {
       title: 'Profile',
       first_name: user.first_name,
-      watch_list: user.watch_list,
-      preferences: user.preferences
+      preferences: user.preferences,
+      watchList: watchList,
+      likedMovies: likedMovies,
+      dislikedMovies: dislikedMovies
     })
   } else {
     //TODO Add a section to display errors in the signup page
@@ -22,5 +49,11 @@ router.get('/', async (req, res) => {
     return
   }
 })
+
+function pushToArray (targetArray, inputArray) {
+  if (inputArray) {
+    inputArray.map(input => targetArray.push(input))
+  }
+}
 
 module.exports = router

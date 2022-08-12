297cd21cd1dc03896cee95f48d3c007c8e0dbacd
David <dfelician.97@gmail.com>
2022-08-12 13:46:06 -0400

    login and express-session (#5)
    * updated getters for username and email
    
    * login and express-session
    
    * Update login.js
    
    added id to req.session.user

--- data/users.js
+++ data/users.js
@@ -3,24 +3,24 @@ const users = mongoCollections.users;
 const { ObjectId } = require("mongodb");
 
 const exportedMethods = {
-  async getUsername(username) {
-    const usersCollection = await users();
-    const user = await usersCollection.findOne({ username: username });
-    return user;
-  },
+    async getUser(username){
+        const usersCollection = await users();
+        const user = await usersCollection.findOne({username: username});
+        return user;
+    },
+    async getUserByEmail(email){
+        const usersCollection = await users();
+        const user = await usersCollection.findOne({email: email});
+        return user;
+    },
+    async insertUser(user){
+        const usersCollection = await users();
+        const newUser = await usersCollection.insertOne(user);
+        return newUser.insertedId;
+    },
   async getUserById(id) {
     const usersCollection = await users();
     const user = await usersCollection.findOne({ _id: ObjectId(id) });
-    return user;
-  },
-  async insertUser(user) {
-    const usersCollection = await users();
-    const newUser = await usersCollection.insertOne(user);
-    return newUser.insertedId;
-  },
-  async getEmail(email) {
-    const usersCollection = await users();
-    const user = await usersCollection.findOne({ email: email });
     return user;
   },
   async getUsersById(idArray) {
---
+++ routes/login.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const user = require('../data/users');
+
+router.get('/', async (req, res) => {
+    res.render('users/login', {title: "Login"});
+});
+
+router.post('/', async(req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+    const userAttempt = await user.getUser(username);
+    if(userAttempt){
+        const hashedPassword = userAttempt.password;
+        if(hashedPassword){
+            if(await bcrypt.compare(password, hashedPassword)){
+                req.session.user = {username: username, id: userAttempt._id};
+                res.redirect('/');
+            }else{
+                let error = "Invalid password";
+                res.status(401).render('users/login', {error: error, title: "Login"});
+            }
+        }
+    }else{
+        let error = "Invalid username";
+        res.status(401).render('users/login', {error: error, title: "Login"});
+    }
+});
+
+module.exports = router;

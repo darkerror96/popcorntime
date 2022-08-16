4be0ed96fa10bddc0ca2a8b22c9374337cd35592
Rut Patel <rutpatel1996@gmail.com>
2022-08-15 21:49:01 -0400

    Fixed Add Movie routing issue for admin user

--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const movies = require("../data/movies");
 
 router.get("/", async (req, res) => {
-  res.render("users/admin", { title: "Admin" });
+  res.render("users/admin", {
+    title: "Manage Movies"
+  });
 });
 
 router.post("/", async (req, res) => {
@@ -11,40 +12,8 @@ router.post("/", async (req, res) => {
   if (option === "edit" || option === "delete") {
     res.redirect("/");
   } else if (option === "add") {
-    res.redirect("/admin/add");
+    res.redirect("/movies/add");
   }
 });
 
-router.get("/add", async (req, res) => {
-  res.render("movies/add_movie", { title: "Add Movie" });
-});
-
-router.post("/add", async (req, res) => {
-  const name = req.body.movie_name;
-  const summary = req.body.summary;
-  const genres = req.body.genres;
-  const duration = req.body.duration;
-  const poster = req.body.poster;
-  const release_date = req.body.release_date;
-  const cast = req.body.cast;
-  const director = req.body.director;
-
-  const newMovie = {
-    name: name,
-    summary: summary,
-    genres: genres,
-    duration: duration,
-    poster: poster,
-    release_date: release_date,
-    cast: cast,
-    director: director,
-  };
-
-  const newMovieId = await movies.addMovie(newMovie);
-  if (newMovieId) {
-    res.redirect("/admin");
-  } else {
-    res.redirect("/admin/add");
-  }
-});
 module.exports = router;

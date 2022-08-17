e65281d43fd5c7aa9f4f78c3c22cbc9fc02b4597
MayankYadav11 <106607153+MayankYadav11@users.noreply.github.com>
2022-08-16 22:58:49 -0400

    Mayank (#67)
    * Added space in main.handlebars
    
    * Updated .gitignore
    
    * Updated links to Wishlist, Profile, Logout page in main.handlebars
    
    * Small ui change
    
    * search page + api url update
    
    Added search/get for movie api
    created routes/movies.js - still needs updating
    updated ui for routes/search.js
    updated ui for views/shows/index.handlebars
    
    * working search page
    
    * getting rid of comments and console logs
    
    * Search work
    
    Combined data/tvshows into data/movies
    Got rid of data/tvshows
    Updated references of shows to movies in routes/search
    Got rid of routes/shows
    Moved shows/index to movies/index
    Updated reference of shows to movies in views/movies/new
    
    * Changes for search page
    
    Added data functions for movie, cast, director in data/movies
    Added drop down menu to select movie, cast, director in routes/index
    Updated search.js to handle searching by movie, cast, director
    Changed name for views/movies/new to view/movies/homePage
    
    * Searching by API changes
    
    Added function to search movie by api in data/movies
    Changed names of views (single & index to moviePage & searchPage)
    
    * search movie by Year addition
    
    * searching using db
    
    * Update movies.js
    
    * Able to search by name, year, cast, director. E2E
    
    * Added edit/delete button for admin on movie page
    
    * Fixed searchYear
    
    * Updates for recommended page
    
    * Update to code for recommendations
    
    Included a fetchCast (need one for movies, directors, genre as well)
    Added a getAllUsers in data/users
    Moved relevant info out of routes/index.js to routes/home + more
    Updated views/movies/homePage to account for preferred actors recommendations
    
    * Recommendation page update
    
    Created fetch... methods to get data in data/movies + formatter messed up stuff
    Code for getting recommendations on routes/home
    UI change for recommendations on view/movies/homePage

--- routes/home.js
+++ routes/home.js
@@ -13,26 +13,110 @@ router.get("/", async (req, res) => {
 
       let likedActors = [];
       const liked_actors = user.preferences.liked_actors;
-      for (var i = 0; i < liked_actors.length; i++) {
-        let temp = await movies.fetchCast(liked_actors[i]);
+      if (liked_actors.length > 0) {
+        for (var i = 0; i < liked_actors.length; i++) {
+          let temp = await movies.fetchCast(liked_actors[i]);
+          pushToArray(likedActors, temp);
+        }
+        if (likedActors.length > 5) {
+          likedActors = getRandom(likedActors, 5);
+        }
+      } else {
+        const allMovies = await movies.getAllMovies();
+        likedActors = allMovies
+          .sort((a, b) => {
+            if (a.avg_rating === b.avg_rating) {
+              return b.reviews.length - a.reviews.length;
+            }
+            return b.avg_rating - a.avg_rating;
+          })
+          .slice(0, 100);
+        likedActors = getRandom(likedActors, 5);
+      }
 
-        pushToArray(likedActors, temp);
-        console.log("Added " + likedActors.length);
-        console.log(likedActors);
+      let likedDirectors = [];
+      const liked_directors = user.preferences.liked_directors;
+      if (liked_directors.length > 0) {
+        for (var i = 0; i < liked_directors.length; i++) {
+          let temp = await movies.fetchDirector(liked_directors[i]);
+          pushToArray(likedDirectors, temp);
+        }
+        if (likedDirectors.length > 5) {
+          likedDirectors = getRandom(likedDirectors, 5);
+        }
+      } else {
+        const allMovies = await movies.getAllMovies();
+        likedDirectors = allMovies
+          .sort((a, b) => {
+            if (a.avg_rating === b.avg_rating) {
+              return b.reviews.length - a.reviews.length;
+            }
+            return b.avg_rating - a.avg_rating;
+          })
+          .slice(0, 100);
+        likedDirectors = getRandom(likedDirectors, 5);
+      }
+
+      let likedGenres = [];
+      const liked_genres = user.preferences.liked_genres;
+      if (liked_genres.length > 0) {
+        for (var i = 0; i < liked_genres.length; i++) {
+          let temp = await movies.fetchGenre(liked_genres[i]);
+          pushToArray(likedGenres, temp);
+        }
+        if (likedGenres.length > 5) {
+          likedGenres = getRandom(likedGenres, 5);
+        }
+      } else {
+        const allMovies = await movies.getAllMovies();
+        likedGenres = allMovies
+          .sort((a, b) => {
+            if (a.avg_rating === b.avg_rating) {
+              return b.reviews.length - a.reviews.length;
+            }
+            return b.avg_rating - a.avg_rating;
+          })
+          .slice(0, 100);
+        likedGenres = getRandom(likedGenres, 5);
       }
 
       res.render("movies/homePage", {
-        title: "loser",
+        title: "Flick Finder",
         option: movieOption,
-        preference: likedActors
+        actorHeading: "By Preferred Actor:",
+        actor: likedActors,
+        directorHeading: "By Preferred Director:",
+        director: likedDirectors,
+        genreHeading: "By Preferred Genre:",
+        genre: likedGenres,
       });
     } else {
-      res.render("movies/homePage", {
-        title: "loser",
-        option: movieOption,
-        // preference: user.preference
-      });
+      let randomRecommendation = [];
+      let likedActors, likedDirectors, likedGenres = [];
+      const allMovies = await movies.getAllMovies();
+        randomRecommendation = allMovies
+          .sort((a, b) => {
+            if (a.avg_rating === b.avg_rating) {
+              return b.reviews.length - a.reviews.length;
+            }
+            return b.avg_rating - a.avg_rating;
+          })
+          .slice(0, 100);
+        likedActors = getRandom(randomRecommendation, 5);
+        likedDirectors = getRandom(randomRecommendation, 5);
+        likedGenres = getRandom(randomRecommendation, 5);
+        res.render("movies/homePage", {
+            title: "Flick Finder",
+            option: movieOption,
+            actorHeading: "By Preferred Actor:",
+            actor: likedActors,
+            directorHeading: "By Preferred Director:",
+            director: likedDirectors,
+            genreHeading: "By Preferred Genre:",
+            genre: likedGenres,      
+        });
     }
+      
   } catch (e) {
     res.status(500).render("movies/error", {
       title: "No Movies Found",
@@ -43,9 +127,15 @@ router.get("/", async (req, res) => {
 });
 
 function pushToArray(targetArray, inputArray) {
-    if (inputArray) {
-      inputArray.forEach((input) => targetArray.push(input));
-    }
+  if (inputArray) {
+    inputArray.forEach((input) => targetArray.push(input));
   }
+}
+
+function getRandom(arr, num) {
+  const shuffled = [...arr].sort(() => 0.5 - Math.random());
+
+  return shuffled.slice(0, num);
+}
 
 module.exports = router;

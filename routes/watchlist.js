608afa11da4b004b406f5e9f6bf9840e6d33282b
HitamYadav <49002850+HitamYadav@users.noreply.github.com>
2022-08-19 11:52:54 -0400

    watchlist button functionality
    creating watchlist

--- routes/watchlist.js
+++ routes/watchlist.js
@@ -11,4 +11,16 @@ router.get('/', async (req, res) => {
     }
 });
 
+// module.exports = router;
+
+// // Updated From Here
+// const express = require('express');
+// const router = express.Router();
+// const movies = require("../data/watchlist");
+// var xss = require("xss");
+
+// router.post('/', async (req, res) => {
+   
+// });
+
 module.exports = router;
